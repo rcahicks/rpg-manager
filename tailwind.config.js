93fe53d0303a14e883d6f17b6ba8bbd57283534e
Christian Hicks <r.ca.hicks@gmail.com>
2025-01-26 14:02:12 -0500

    Update design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      'sm': '.8rem',
+      'md': '1rem',
+      'lg': '1.5rem',
+      'xl': '2.5rem',
+      '2xl': '4rem'
+    },
     extend: {
+      backgroundImage: {
+        'main': "url('../Assets/Images/background-img.jpg')",
+      }
     },
   },
   plugins: [],
